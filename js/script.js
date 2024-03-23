14abd28ddad7495df5f50ab1acd6f907d2ceb7f1
Malin Håkansson <malin.hson@gmail.com>
2024-03-23 19:43:02 +0100

    Function to check players input

--- js/script.js
+++ js/script.js
@@ -11,13 +11,14 @@ const remainingGuesses = document.querySelector(".remaining");
 //The span inside the paragraph where the remaining guesses will display
 const remainingGuessesSpan = document.querySelector("span");
 //The empty paragraph where messages will appear when the player guesses a letter
-const messagesAppear = document.querySelector(".message");
+const messageAppear = document.querySelector(".message");
 //The hidden button that will appear prompting the player to play again
 const playAgainButton = document.querySelector(".play-again hide")
 //Starting word to test out the game
 const word = "magnolia";
 
 //Function to Add Placeholders for Each Letter
+//explanation of the circles for word: So with your placeholder function, you're first creating an empty array called placeholderLetters . Then your for loop is saying "for every letter that exists in word , I'm going to put in a new circle into the placeholder array. So you're not tampering with the actual word , you're creating a new array and creating a circle every time it loops through a letter from the word and it loops until it goes through each letter then stops! So you end up with the exact amount of circles that existed in the word
 const placeholders = function(word){
     //Empty array to be filled with dots
     const placeholderLetters = [];
@@ -31,7 +32,6 @@ const placeholders = function(word){
 
 placeholders(word);
 
-//explanation of the circles for word: So with your placeholder function, you're first creating an empty array called placeholderLetters . Then your for loop is saying "for every letter that exists in word , I'm going to put in a new circle into the placeholder array. So you're not tampering with the actual word , you're creating a new array and creating a circle every time it loops through a letter from the word and it loops until it goes through each letter then stops! So you end up with the exact amount of circles that existed in the word
 guessButton.addEventListener("click", function(e){
     //Prevents default behaviour of clicking button, form submitting and reloading page
     e.preventDefault();
@@ -40,3 +40,18 @@ guessButton.addEventListener("click", function(e){
     letterInput.value = "";
 });
 
+//Checking players input
+const validateInput = function(input){
+    //regular expression to ensure input is a letter
+    const acceptedLetter = /[a-zA-Z]/;
+    //is input empty?
+    if (input.length === 0) {
+        messageAppear.innerText = "Try another letter!"
+    } else if (input.length > 1) {
+        messageAppear.innerText = "Please only one letter at a time."
+    } else if (!input.match(acceptedLetter)) {
+        messageAppear.innerText = "Letters only, pleaase."
+    } else {
+        return input;
+    }
+};
